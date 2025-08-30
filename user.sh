103ad3a14739f8c0e6cf2e0d14ca981504145cc0
sanu15 <71996077+sanu15@users.noreply.github.com>
2025-08-30 08:49:42 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -5,8 +5,9 @@ read -p "Enter username: " username
 read -s -p "Enter password: " password
 echo
 
-# Check credentials
-if [ "$username" == "admin" ] && [ "$password" == "testyantra123" ]; then
+# Authentication logic
+if { [ "$username" == "admin" ] && [ "$password" == "testyantra123" ]; } || \
+   { [ "$username" == "Developer" ] && [ "$password" == "Fireflink@123" ]; }; then
     echo "Authentication Successful ✅"
 else
     echo "Authentication Unsuccessful ❌"
