fe28f17764a6a5dc6cb02c60eacb263dbef7522d
sahana <ksahana1006@gmail.com>
2025-08-30 03:23:01 +0000

    modified age files

--- age.sh
+++ age.sh
@@ -16,6 +16,8 @@ if [ "$age" -lt 13 ]; then
     echo "You are a Child."
 elif [ "$age" -ge 13 ] && [ "$age" -le 19 ]; then
     echo "You are a Teenager."
-else
+elif [ "$age" -ge 20 ] && [ "$age" -le 60 ]; then
     echo "You are an Adult."
+else
+    echo "You are in Oldage."
 fi
